3c1590358bbdbd66a7e1f563def9059ba763f8b2
Sashko Stubailo <sashko@stubailo.com>
2017-03-24 21:56:06 -0700

    Add a buncha loaders

--- gen/webpack.config.js
+++ gen/webpack.config.js
@@ -1,7 +1,29 @@
+const path = require('path');
+
+const nm = path.join(__dirname, '..', 'node_modules');
+
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'index.js',
     path: `${process.cwd()}/../build`
+  },
+  resolveLoader: { modules: [nm] },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              path.join(nm, 'babel-preset-env'),
+              path.join(nm, 'babel-preset-react')
+            ]
+          }
+        }
+      }
+    ]
   }
 };
