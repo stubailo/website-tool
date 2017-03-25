5bfc39481877464f5c8d53e046eeced99b7a0893
Sashko Stubailo <sashko@stubailo.com>
2017-03-24 23:42:35 -0700

    Fix, we had the wrong version fml

--- gen/webpack.config.js
+++ gen/webpack.config.js
@@ -30,8 +30,8 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              'babel-preset-env',
-              'babel-preset-react'
+              require.resolve('babel-preset-env'),
+              require.resolve('babel-preset-react')
             ]
           }
         }
