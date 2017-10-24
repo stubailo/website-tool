e44a16bf594d5f24ca3be863b766690baa4c1e46
Sashko Stubailo <sashko@meteor.com>
2017-10-24 00:07:28 -0700

    Attempt to add less loader

--- gen/webpack.config.js
+++ gen/webpack.config.js
@@ -35,7 +35,17 @@ module.exports = {
             ]
           }
         }
-      }
+      },
+      {
+        test: /\.less$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "less-loader" // compiles Less to CSS
+        }]
+    }
     ]
   }
 };
